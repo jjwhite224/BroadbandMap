ffc5f78fe666fc1e241b31394a9c586eb1791df3
jjwhite224 <56843367+jjwhite224@users.noreply.github.com>
2025-02-11 11:37:15 -0500

    Update interactivemap.js

--- interactivemap.js
+++ interactivemap.js
@@ -82,12 +82,11 @@ function onEachFeature(feature, layer) {
 }
 function zoomToZip(zipInput) {
     let foundFeature = null; // Declare the variable outside
-   
+    let cleanedZip = zipInput.trim();                                                                                                                                                                                                           
     geoJsonLayer.eachLayer(layer => {
         console.log(layer.feature.properties.ZCTA5CE20)
-        if (layer.feature && layer.feature.properties.ZCTA5CE20 && 
-            layer.feature.properties.ZCTA5CE20.toString() === zipInput) {
-          
+        if (layer.feature && layer.feature.properties.ZCTA5CE20.toString() && 
+            layer.feature.properties.ZCTA5CE20.toString() === cleanedZip) {
             foundFeature = layer;
         }
     });
